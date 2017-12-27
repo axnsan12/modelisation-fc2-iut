9247ef8207cbaa465fd01632e6efa42ca52d57f3
agnerayq <agnerayq@agnerayq-To-be-filled-by-O-E-M>
2017-12-27 14:41:13 +0100

    Add menu and menubar

--- src/modelisation/Window.java
+++ src/modelisation/Window.java
@@ -2,6 +2,9 @@ package modelisation;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,7 +16,40 @@ public class Window extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+    	
+    	
+    	 // Creation Menubar
+        MenuBar menuBar = new MenuBar();
+        
+        // Creation menus
+        Menu fichierMenu = new Menu("Fichier");
+        Menu fonctionsMenu = new Menu("Fonctions");
+        Menu aideMenu = new Menu("Aide");
+        
+        // Creation menu item
+        MenuItem nouveauItem = new MenuItem("Nouveau");
+        MenuItem ouvertureFichierItem = new MenuItem("Ouvrir fichier CSV");
+        MenuItem enregistreFichierItem = new MenuItem("Enregistrer arbre");
+        MenuItem quitterItem = new MenuItem("Quitter");
+        
+        MenuItem giniItem = new MenuItem("coefficient de Gini");
+        MenuItem khideuxItem = new MenuItem("khi-deux");
+        MenuItem entropieItem = new MenuItem("entropie");
+        
+        // Ajout menu item au menu
+        fichierMenu.getItems().addAll(nouveauItem, ouvertureFichierItem,enregistreFichierItem, quitterItem);
+        fonctionsMenu.getItems().addAll(giniItem, khideuxItem,entropieItem);
+        
+        // Ajout du menu à la barre de Menu
+        menuBar.getMenus().addAll(fichierMenu, fonctionsMenu, aideMenu);
+    	
+    	
+    	
     	AnchorPane root = new AnchorPane();
+    	root.setTopAnchor(menuBar,  10.0);
+        root.setLeftAnchor(menuBar, 10.0);
+        AnchorPane.setRightAnchor(menuBar, 10.0);
+        root.getChildren().add(menuBar);
     	Scene scene = new Scene(root, 400, 200);
         primaryStage.setTitle("Arbre");
         primaryStage.setScene(scene);

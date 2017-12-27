4cb329f285410bdf593ed6e3a69c6923f3fe1580
agnerayq <agnerayq@agnerayq-To-be-filled-by-O-E-M>
2017-12-27 14:35:31 +0100

    display blank window

--- src/modelisation/Window.java
+++ src/modelisation/Window.java
@@ -1,6 +1,8 @@
 package modelisation;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Window extends Application {
@@ -11,6 +13,10 @@ public class Window extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+    	AnchorPane root = new AnchorPane();
+    	Scene scene = new Scene(root, 400, 200);
+        primaryStage.setTitle("Arbre");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }

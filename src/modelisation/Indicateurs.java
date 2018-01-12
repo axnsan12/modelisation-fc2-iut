bce44aa772a9565b60281174dc0324e8f4aac5ed
Caroline Mathon <caroline.mathon@etudiant.univ-lille1.fr>
2018-01-12 10:38:54 +0100

    Indicateurs finis

--- src/modelisation/Indicateurs.java
+++ src/modelisation/Indicateurs.java
@@ -2,7 +2,15 @@ package modelisation;
 
 public class Indicateurs {
 	
+	//Arbre de classification
+	
 	//Retourne le chi de deux vecteurs passes en parametres
+	/**
+	 * 
+	 * @param X vecteur d'entiers
+	 * @param Y vecteur d'entiers
+	 * @return Retourne le coefficient chi2 des deux vecteurs.
+	 */
 	public static double chi2(int []X, int[] Y)
 	{
 		int [][] tab = tabCont(X,Y);
@@ -104,6 +112,152 @@ public class Indicateurs {
 		return Xbis;
 	}
 	
+	//Afficher le tableau de contingence de deux vecteurs passes en parametres 
+			public static void afficherTabCont(int []X, int[] Y)
+			{
+				int [][] tabCont = tabCont(X,Y);
+				int opt1 = 0;
+				System.out.print("|___ /");
+				for (int j=1;j<tabCont[0].length;j++)
+				{
+					System.out.print("|" + j + " ");
+				}
+				System.out.println("|Total|");
+				for (int i=1;i<tabCont.length+1;i++)
+				{
+					if (i == tabCont.length)
+					{
+						System.out.print("|Total");
+					}
+					else
+					{
+						System.out.print("|0000" + i);
+					}
+					
+					for (int j=1;j<tabCont[0].length+1;j++)
+					{
+						System.out.print("|" +tabCont[i-1][j-1] + " ");
+					}
+					if (i == tabCont.length)
+					{
+						System.out.print("| ");
+					}
+					else
+					{
+						System.out.println("| ");
+					}
+				}
+				
+			}
+			
+			//Renvoie la table de contingence de deux vecteurs passes en parametres
+			private static int[][] tabCont(int[] X, int[] Y) {
+				int [][] tab;
+				int []totopt1=calculOption(X);
+				int []totopt2=calculOption(Y);
+				int som=0;
+				
+				tab = new int[totopt2.length+1][totopt1.length+1];
+				
+				for (int i=0;i<totopt2.length+1;i++)
+				{
+					for(int j=0;j<totopt1.length+1;j++)
+					{
+						tab[i][j] = 0;
+					}
+				}
+				for (int k=0;k<X.length;k++)
+				{
+					tab[Y[k]-1][X[k]-1] = tab[Y[k]-1][X[k]-1]+1;
+					tab[Y[k]-1][totopt1.length] = tab[Y[k]-1][totopt1.length]+1;
+					tab[totopt2.length][X[k]-1] = tab[totopt2.length][X[k]-1]+1;
+					tab[totopt2.length][totopt1.length]=tab[totopt2.length][totopt1.length]+1;
+				}
+				return tab;
+			}
+	
+	//Arbre de regression
+			
+			/**
+			 * 
+			 * @param i indice teste
+			 * @param Y Variable cible
+			 * @param X Variable a etudier
+			 * @return
+			 */
+	public static double d(int i, int []Y, int [] X)
+	{
+		double [] fils1 = moyenneFilsSup(i,Y,X);
+		double [] fils2 = moyenneFilsInf(i,Y,X);
+		return fils1[1] + fils2[1];
+		
+	}
+			
+			/**
+			 * Min
+			 * @param i indice teste
+			 * @param Y Variable cible
+			 * @param X Variable a etudier
+			 * @return
+			 */
+	private static double [] moyenneFilsSup(int i, int []Y, int [] X)
+	{
+		double c = C(i,X);
+		double moyenne=0,cmpt=0,d=0,sum1=0,sum2=0;
+		double combi[] = new double[2];
+		for (int j=0;j<Y.length;j++)
+		{
+			if (X[j] >= c)
+			{
+				cmpt ++;
+				moyenne = moyenne + (double)Y[j];
+				sum1 = sum1 + (double)Y[j];
+				sum2 = sum2 + (double)Y[j]*Y[j];
+			}
+		}
+		moyenne = moyenne/cmpt;
+		d = moyenne*moyenne +((sum2-2*moyenne*sum1)/cmpt);
+		combi[0]=moyenne;
+		combi[1]=d;
+		return combi;
+	}
+	
+			/**
+			 * 
+			 * @param i indice teste
+			 * @param Y Variable cible
+			 * @param X Variable a etudier
+			 * @return
+			 */
+	private static double[] moyenneFilsInf(int i, int []Y, int [] X)
+	{
+		double c = C(i,X);
+		double moyenne=0,cmpt=0,d=0,sum1=0,sum2=0;
+		double combi[] = new double[2];
+		for (int j=0;j<Y.length;j++)
+		{
+			if (X[j] < c)
+			{
+				cmpt ++;
+				moyenne = moyenne + (double)Y[j];
+				sum1 = sum1 + (double)Y[j];
+				sum2 = sum2 + (double)Y[j]*Y[j];
+			}
+		}
+		moyenne = moyenne/cmpt;
+		d = moyenne*moyenne +((sum2-2*moyenne*sum1)/cmpt);
+		combi[0]=moyenne;
+		combi[1]=d;
+		return combi;
+	}
+	
+	private static double C(int i,int[] X)
+	{
+		return ((double)X[i]+(double)X[i-1]/2);
+	}
+	
+	//Outils internes
+	
 	private static int minimum(int[] x) {
 		int min=x[0];
 		for (int i=1;i<x.length;i++)
@@ -112,6 +266,7 @@ public class Indicateurs {
 		}
 		return min;
 	}
+	
 
 	private static int maximum(int[] x) {
 		// TODO Auto-generated method stub
@@ -123,69 +278,7 @@ public class Indicateurs {
 		return max;
 	}
 
-	//Afficher le tableau de contingence de deux vecteurs passes en parametres 
-		public static void afficherTabCont(int []X, int[] Y)
-		{
-			int [][] tabCont = tabCont(X,Y);
-			int opt1 = 0;
-			System.out.print("|___ /");
-			for (int j=1;j<tabCont[0].length;j++)
-			{
-				System.out.print("|" + j + " ");
-			}
-			System.out.println("|Total|");
-			for (int i=1;i<tabCont.length+1;i++)
-			{
-				if (i == tabCont.length)
-				{
-					System.out.print("|Total");
-				}
-				else
-				{
-					System.out.print("|0000" + i);
-				}
-				
-				for (int j=1;j<tabCont[0].length+1;j++)
-				{
-					System.out.print("|" +tabCont[i-1][j-1] + " ");
-				}
-				if (i == tabCont.length)
-				{
-					System.out.print("| ");
-				}
-				else
-				{
-					System.out.println("| ");
-				}
-			}
-			
-		}
-		
-		//Renvoie la table de contingence de deux vecteurs passes en parametres
-		private static int[][] tabCont(int[] X, int[] Y) {
-			int [][] tab;
-			int []totopt1=calculOption(X);
-			int []totopt2=calculOption(Y);
-			int som=0;
-			
-			tab = new int[totopt2.length+1][totopt1.length+1];
-			
-			for (int i=0;i<totopt2.length+1;i++)
-			{
-				for(int j=0;j<totopt1.length+1;j++)
-				{
-					tab[i][j] = 0;
-				}
-			}
-			for (int k=0;k<X.length;k++)
-			{
-				tab[Y[k]-1][X[k]-1] = tab[Y[k]-1][X[k]-1]+1;
-				tab[Y[k]-1][totopt1.length] = tab[Y[k]-1][totopt1.length]+1;
-				tab[totopt2.length][X[k]-1] = tab[totopt2.length][X[k]-1]+1;
-				tab[totopt2.length][totopt1.length]=tab[totopt2.length][totopt1.length]+1;
-			}
-			return tab;
-		}
+	
 
 		//Renvoie un vecteur contenant la liste des options possibles.
 		private static int[] calculOption(int[] x) {

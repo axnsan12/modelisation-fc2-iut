3750b7edb8b72831b14c1926faab4b341f19db79
Cristi Vîjdea <cristian.vijdea@etudiant.univ-lille1.fr>
2018-01-12 11:48:59 +0100

    [WIP] Tree building algorithm

--- src/modelisation/builder/DecisionTreeBuilder.java
+++ src/modelisation/builder/DecisionTreeBuilder.java
@@ -3,6 +3,10 @@ package modelisation.builder;
 import modelisation.Indicateurs;
 import modelisation.tree.DecisionTree;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+
 public class DecisionTreeBuilder {
     protected int idColumnIndex, targetColumnIndex;
 
@@ -11,26 +15,84 @@ public class DecisionTreeBuilder {
         this.targetColumnIndex = targetColumnIndex;
     }
 
+    protected static class SplitScore
+    {
+        /**
+         *
+         */
+        public final int columnIndex;
+
+        /**
+         *
+         */
+        public final double splitValue;
+
+        /**
+         *
+         */
+        public final double score;
+
+        public SplitScore(int columnIndex, double splitValue, double score) {
+            this.columnIndex = columnIndex;
+            this.splitValue = splitValue;
+            this.score = score;
+        }
+    }
+
     /**
      * Generate a decision tree from some input data.
      *
-     * @param data input data as a column matrix, i.e. an array of columns - data[i][j] is the `j`th row of column `i`
+     * @param data input data as a column-indexed matrix, i.e. an array of columns - data[i][j] is the `j`th row
+     *             of column `i`, and `data.length` is the number of columns
      * @return the built decision tree
      */
     public DecisionTree buildTree(int[][] data) {
         checkInputData(data, idColumnIndex, targetColumnIndex);
 
+        ArrayList<SplitScore> scores = new ArrayList<>(data.length);
+        // we go through all unused columns and calculate the split score for each
         for (int columnIndex = 0; columnIndex < data.length; ++columnIndex) {
             if (columnIndex != idColumnIndex && columnIndex != targetColumnIndex) {
-                
+                final double splitValue;
+                if (Indicateurs.shouldDiscretize(data[columnIndex])) {
+                    splitValue = Indicateurs.getSplitValue(data[columnIndex]);
+                }
+                else {
+                    splitValue = Double.NaN;
+                }
+                double score = evaluateSplit(data[targetColumnIndex], data[columnIndex]);
+                scores.add(new SplitScore(columnIndex, splitValue, score));
             }
         }
+
+        SplitScore split = chooseBestScore(scores);
+
         return null;
     }
 
+    /**
+     * Choose the best split from a collection of splits scored by {@link #evaluateSplit(int[], int[])}.
+     *
+     * The score returned by {@link #evaluateSplit(int[], int[])} is accesible via {@link SplitScore#score}.
+     *
+     * @param splits
+     * @return
+     */
+    protected SplitScore chooseBestScore(Collection<SplitScore> splits) {
+        return splits.stream().min(Comparator.comparingDouble(s -> s.score))
+                .orElseThrow(() -> new IllegalArgumentException("empty splits array"));
+    }
 
-    protected double evaluateSplit(int[][] data, int columnIndex) {
-        return Indicateurs.gini(data[idColumnIndex], data[columnIndex]);
+    /**
+     * Return a score for the possible prediction accuracy gains against `targetColumn`
+     * by introducing a splitter node on the given `splitColumn`.
+     *
+     * @param targetColumn column containing the values to classify/predict
+     * @param splitColumn column containing discrete values to make a split decision on
+     * @return a split score as interpreted by {@link #chooseBestScore(Collection)}
+     */
+    protected double evaluateSplit(int[] targetColumn, int[] splitColumn) {
+        return Indicateurs.gini(targetColumn, splitColumn);
     }
 
     private static void checkInputData(int[][] data, int idColumnIndex, int targetColumnIndex) throws IllegalArgumentException {
--- src/modelisation/tree/DecisionTree.java
+++ src/modelisation/tree/DecisionTree.java
@@ -1,5 +1,20 @@
 package modelisation.tree;
 
-public class DecisionTree {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class DecisionTree {
+    public static class Node {
+        private Node parent;
+        private ArrayList<Node> children;
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public List<Node> getChildren() {
+            return Collections.unmodifiableList(children);
+        }
+    }
 }
